c12c0eea8f02bf13ebc6e66c48914c776641120e
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-11-12 23:36:02 -0800

    build Client, Animal Type View is broken

---
+++ AnimalShelterClient/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using AnimalShelterClient.Models;
+
+namespace AnimalShelterClient.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        var allAnimals = Animal.GetAnimals();
+        return View(allAnimals);
+    }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+        var targetAnimal = Animal.PutAnimal(animal);
+        return RedirectToAction("Details", "Animals", targetAnimal);
+    }
+
+    public ActionResult Details (Animal animal)
+    {
+        return View(animal);
+    }
+
+    [HttpGet]
+    public ActionResult Type(Animal animal)
+    {
+        var animalList = Animal.GetAnimals();
+        return View(animalList);
+    }
+
+  }
+}
+
